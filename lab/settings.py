22bf054a5fdd344a0394b06a6760e7fe70521828
imroz_khan <kimu7466@gmail.com>
2024-01-02 09:44:06 +0530

    update

--- lab/settings.py
+++ lab/settings.py
@@ -132,4 +132,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kimu7466@gmail.com'
-EMAIL_HOST_PASSWORD = 'jpcgjperrhgxqqwq'
+EMAIL_HOST_PASSWORD = 'gphpwqucokjqxask'
